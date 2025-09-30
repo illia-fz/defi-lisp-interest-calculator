da93ee294529bc189238f84197399d992dd336fa
Illia <77698519+illia-fz@users.noreply.github.com>
2025-09-30 04:17:02 -0700

    Create main.lisp

---
+++ main.lisp
@@ -0,0 +1,9 @@
+;; Compound interest calculator in Lisp
+(defun compound (principal rate periods)
+  (* principal (expt (+ 1 rate) periods)))
+
+(let* ((principal 1000)
+       (rate 0.05)
+       (periods 5)
+       (result (compound principal rate periods)))
+  (format t "~%Compound interest result: ~a~%" result))
